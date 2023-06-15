4d98661143e3fb0fa19028815f0001cd51dc4057
Leonardo Henke <98836382+LeonardoHenke@users.noreply.github.com>
2023-06-15 12:08:52 -0300

    add versão mobile

--- codigo.py
+++ codigo.py
@@ -1,9 +1,26 @@
 import pyautogui
 from time import sleep
 
-x, y = 557, 125
-for i in range (0, 34):
+letras = 'askdjalskjdkasjdlkajskljfrhhbcamzjznhcasdasdfasdfasdfagdafgazxcbfghaseasdba'
+
+cont = 0
+
+# x, y = 557, 140
+# for i in range (0, 34):
+#     for i in letras:
+#         pyautogui.click(x, y, duration=0.3)
+#         pyautogui.write(i)
+#         pyautogui.press('enter')
+#         sleep(2)
+
+
+x, y = 557, 190
+for i in letras:
     pyautogui.click(x, y, duration=0.3)
-    pyautogui.write('a')
+    pyautogui.write(i)
     pyautogui.press('enter')
+    cont += 1
+    if cont == 22:
+        break
     sleep(1)
+
