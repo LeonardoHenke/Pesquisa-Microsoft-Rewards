299ca8136241366495b4c7e1c0d0fe023a2dcbdb
Leonardo Henke <98836382+LeonardoHenke@users.noreply.github.com>
2023-06-22 14:00:35 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -5,22 +5,24 @@ letras = 'askdjalskjdkasjdlkajskljfrhhbcamzjznhcasdasdfasdfasdfagdafgazxcbfghase
 
 cont = 0
 
-# x, y = 557, 140
-# for i in range (0, 34):
-#     for i in letras:
-#         pyautogui.click(x, y, duration=0.3)
-#         pyautogui.write(i)
-#         pyautogui.press('enter')
-#         sleep(2)
-
-
-x, y = 557, 190
+x, y = 557, 155
 for i in letras:
     pyautogui.click(x, y, duration=0.3)
     pyautogui.write(i)
     pyautogui.press('enter')
     cont += 1
-    if cont == 22:
+    if cont == 34:
         break
-    sleep(1)
+    sleep(1.2)
+
+
+# x, y = 557, 200
+# for i in letras:
+#     pyautogui.click(x, y, duration=0.3)
+#     pyautogui.write(i)
+#     pyautogui.press('enter')
+#     cont += 1
+#     if cont == 20:
+#         break
+#     sleep(1.2)
 
